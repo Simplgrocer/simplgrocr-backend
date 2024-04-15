0323910382e747d96e0227817d7da78b95efc287
tirthya_kamal_dasgupta <tirthya.dasgupta@alliedmedia.com>
2024-04-15 15:57:13 +0530

    feat: Added total price columnin grocery list model.

--- grocery_list/models.py
+++ grocery_list/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class GroceryList(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(null=True, blank=True)
+    total_price = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
